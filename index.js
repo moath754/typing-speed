31cd1ab6fe172b50448949741b8a145e797a7d5c
moath754 <moath141400@gmail.com>
2025-11-07 22:20:28 +0300

    Add bot configuration to index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  Token: 'MTQwODg0MjY1NTk4MjQyNDA2NA.GomeDG.jpl49itJFT_JixgiKWYwRjDv-hd9Sp28jrTIs8', // توكن البوت
+  Guild: '1415105760265769145', // ايدي السيرفر
+  Channel: '1435454733459525682', // ايدي القناة الصوتية
+  selfMute: true,  // true = كتم المايك عند الدخول، false = تشغيل المايك
+  selfDeaf: false  // true = تعطيل سماع الصوت، false = سماع الصوت مفعل
+};
